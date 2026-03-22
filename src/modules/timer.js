b40d39fe00d8330ecfd58b351503fb601ed5bf5b
sasha <199w29c@mail.ru>
2026-03-22 22:59:21 +0300

    refactor

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,20 +1,26 @@
+import { formatNumber } from "./utils";
 const deadline = new Date('2026-05-31T23:59:59');
+const hours = document.getElementById('timer-hours');
+const minutes = document.getElementById('timer-minutes');
+const seconds = document.getElementById('timer-seconds')
+
 
         function updateTimer() {
             const now = new Date();
+
             const timeRemaining = deadline - now;
             if (timeRemaining <= 0) {
-                document.getElementById('timer-hours').textContent = '00';
-                document.getElementById('timer-minutes').textContent = '00';
-                document.getElementById('timer-seconds').textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
                 return;
             }
             const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
             const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-            document.getElementById('timer-hours').textContent = hours.toString().padStart(2, '0');
-            document.getElementById('timer-minutes').textContent = minutes.toString().padStart(2, '0');
-            document.getElementById('timer-seconds').textContent = seconds.toString().padStart(2, '0');
+            hours.textContent = formatNumber(hours);
+            minutes.textContent = formatNumber(minutes);
+            seconds.textContent = formatNumber(seconds);
         }
         updateTimer();
         setInterval(updateTimer, 1000); 
